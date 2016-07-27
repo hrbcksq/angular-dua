913ac9117f8db8f8864a5045ea4b192d431843e6
hrbcksq <hrbcksq@gmail.com>
2016-07-27 04:07:37 +0300

    Two-way binding with syntax sugar: [(ngModel)] directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,10 @@ import {AuthorsComponent} from './authors.component'
     selector: 'my-app',
     template: `
     <input type="text" [value]="title" (input)="title = $event.target.value"/>
+    <input type="text" [(ngModel)]="title" />
+    <input type="text" bindon-ngModel="title" />
     Preview: {{title}}
+    <button class='btn btn-primary' (click)='title = ""'>clear</button>
     `,
     directives: [CoursesComponent, AuthorsComponent]
 })
