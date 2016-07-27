f22a6f433cd5a518977e92b55b628fa2d5a28881
hrbcksq <hrbcksq@gmail.com>
2016-07-27 03:35:35 +0300

    Event binding with stop propagation bubble up events

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,19 @@ import {AuthorsComponent} from './authors.component'
 @Component({
     selector: 'my-app',
     template: `
+    <div (click)='onDivClick()'>
         <button class='btn btn-primary' (click)="onClick($event)">Submit</button>
+    </div>
     `,
     directives: [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent {
+    onDivClick($event){
+        console.log('Handled by div');
+    }
+
     onClick($event){
+        $event.stopPropagation();
         console.log('Clicked', $event);
     }    
  }
