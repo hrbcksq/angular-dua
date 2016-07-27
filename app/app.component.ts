b6ec7df3808f03d25e7b29f8644f26a457caea5a
hrbcksq <hrbcksq@gmail.com>
2016-07-27 03:22:25 +0300

    Class binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,14 +4,11 @@ import {AuthorsComponent} from './authors.component'
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1>
-        <h1 [textContent]='title'></h1>       
-
-        <img [src]='source'/>
-        <img src='{{source}}'/>`,
+    template: `
+        <button class='btn btn-primary' [class.active]='isActive'>Submit</button>    
+    `,
     directives: [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent {
-    private title: string = 'Angular 2 logo';
-    private source: string = 'http://code-maven.com/img/angularjs.png';
+    private isActive: boolean = false;    
  }
