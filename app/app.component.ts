b9e69470b8d4016b466ae52dcec4fd78a20156ec
hrbcksq <hrbcksq@gmail.com>
2016-09-03 13:13:44 +0300

    *nfIf usage example, compare with hidden manipulations

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,25 @@
 import {Component} from 'angular2/core';
-import {FavoriteComponent} from './favorite.component'
-import {LikeComponent} from './like.component'
-import {VoterComponent} from './voter.component'
-import {StoryboardComponent} from './storyboard.component'
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.template.html',
-    styleUrls:['app/app.style.css'],
-    directives: [FavoriteComponent, LikeComponent, VoterComponent, StoryboardComponent]
+    // templateUrl: 'app/app.template.html',
+    template: `
+        <input type='button' class='btn btn-primary' value='button' (click)='onClick()'/>
 
+        <div *ngIf='visibility'>
+            This is vertified content
+        </div>
+
+        <div *ngIf="!visibility">
+            This is unvertified content
+        </div>        
+        `   
+        
 })
 export class AppComponent {
-    title = "Angular"    
-    post = {
-        title: "Title",
-        isFavorite: true,
-        isLiked: true,
-        likesCount: 10,
-        vote: 0,
-        votesCount: 24
-    }
+    private visibility: Boolean = true;
 
-    onFavorite(){
-        this.post.isFavorite = !this.post.isFavorite;        
-    }
-
-    onLike(){
-        var post = this.post;
-        post.isLiked = !post.isLiked;
-        post.likesCount += post.isLiked ? 1 : -1;         
-    }
-    
-    onVote(arg){
-        this.post.vote += arg.step;
-        this.post.votesCount += arg.step;        
-    }
+    onClick(){
+        this.visibility = !this.visibility;
+    }    
 }
