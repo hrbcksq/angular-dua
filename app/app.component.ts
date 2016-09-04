78bde18d4172af4920e7630a3e83a93d4d54e112
hrbcksq <hrbcksq@gmail.com>
2016-09-04 15:40:04 +0300

    Creating summary custom pipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,23 @@
 import {Component} from 'angular2/core';
-
+import {SummaryPipe} from './summary.pipe'
 
 @Component({
     selector: 'my-app',    
     template: `                        
             {{course.title | uppercase}}
             <br>
-            {{course.students | number}}
-            <br>
-            {{course.rating | number:'2.2-2'}}
-            <br>            
-            {{course.price | currency:'AUD':true:'2.2-2'}}
-            <br>
-            {{course.releaseDate | date:'MMM yyyy'}}
-            <br>
-            {{course | json}}
-            <br>
-        `          
+            {{course.body | summary:100}}
+        `,
+    pipes: [SummaryPipe]
 })
 export class AppComponent {
     private course = {
         title: "Angular 2 for Beginners",
-        rating: 4.9745,
-        students: 5991,
-        price: 99.95,
-        releaseDate: new Date(2016, 3, 1)        
+        body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`    
     }
 }
