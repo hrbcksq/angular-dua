2a0256a91a7eca4673792f325b86fbc7233b2aa1
hrbcksq <hrbcksq@gmail.com>
2016-09-05 22:24:08 +0300

    ngControl with implisit validation

--- app/contact-form.component.ts
+++ app/contact-form.component.ts
@@ -2,7 +2,8 @@ import { Component } from 'angular2/core';
 
 @Component({    
     selector: 'contact-form',
-    templateUrl: 'app/contact-form.component.html'    
+    templateUrl: 'app/contact-form.component.html',
+    styleUrls: ['app/contact-form.component.css']        
 })
 export class ContactFormComponent {
     log(value){
