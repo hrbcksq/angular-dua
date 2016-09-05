2091ddfcbde5b10e7ca1f28f86aa394aabb5e734
hrbcksq <hrbcksq@gmail.com>
2016-09-05 20:45:47 +0300

    ngForm and ngContol Directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,12 @@
 import {Component} from 'angular2/core';
-import {ZippyPanel} from './zippy.component'
+import {ContactFormComponent} from './contact-form.component'
 
 @Component({
     selector: 'my-app',    
     template: `                        
-            <zippy-panel title='Manual Reloading'>
-                If the streams support doesn't suit your needs, you can fire the reload method manually by wrapping it in a task. This example will compile & auto-inject CSS just as before, but when HTML files are changed, the browsers will be reloaded instead.
-            </zippy-panel>
+            <contact-form></contact-form>
         `,    
-    directives: [ZippyPanel]
+    directives: [ContactFormComponent]
 })
 export class AppComponent {
     
