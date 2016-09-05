a1c051fbc950c004d3b4c7c14b4ef212a6198b97
hrbcksq <hrbcksq@gmail.com>
2016-09-05 12:57:10 +0300

    Zippy exercise correctives

--- app/zippy.component.ts
+++ app/zippy.component.ts
@@ -5,19 +5,13 @@ import {Component, Input} from 'angular2/core';
     template: `
         <div class="panel panel-default">
             <div class="panel-heading" (click)='onExpand()' [ngStyle]="{
-                'border-width' : expanded ? '1px' : '0px' 
-            }">            
-                <div class='row'>
-                    <div class='col-md-11'>{{ title }}</div>
-                    <div class='col-md-1 text-right'>
-                        <i class='glyphicon' [ngClass]="{                            
-                            'glyphicon-chevron-up' : expanded,
-                            'glyphicon-chevron-down' : !expanded}"></i>                        
-                    </div> 
-                </div>
-                                               
+                'border-width' : expanded ? '1px' : '0px'}">            
+                {{ title }}
+                <i class='pull-right glyphicon' [ngClass]="{                            
+                    'glyphicon-chevron-up' : expanded,
+                    'glyphicon-chevron-down' : !expanded}"></i>                                               
             </div>
-            <div class="panel-body" [hidden]='!expanded'>                    
+            <div class="panel-body" *ngIf='expanded'>                    
                 <ng-content></ng-content>
             </div>
         </div>`
