df04dbbbb0032ba5d7dcd1be0db5f5ea4bdabfe9
hrbcksq <hrbcksq@gmail.com>
2016-09-05 09:36:32 +0300

    Elvis operator example

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,22 @@
 import {Component} from 'angular2/core';
-import {SummaryPipe} from './summary.pipe'
+import {SummaryPipe} from './summary.pipe';
+import {FavoriteComponent} from './favorite.component';
 
 @Component({
     selector: 'my-app',    
     template: `                        
-            {{course.title | uppercase}}
-            <br>
-            {{course.body | summary:100}}
+            <ul>
+                <li>Title: {{ task.title }}</li>
+                <li>Assigned to: {{ task.assignee?.name }}</li>
+            </ul>
         `,
-    pipes: [SummaryPipe]
+    pipes: [SummaryPipe],
+    directives: [FavoriteComponent]
 })
-export class AppComponent {
-    private course = {
-        title: "Angular 2 for Beginners",
-        body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`    
+export class AppComponent {    
+    task = {
+        title: 'Review applications',
+        assignee: null
     }
+    
 }
