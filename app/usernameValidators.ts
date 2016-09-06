54cbac286389c637ce3bb10695dc88f7c5fb9eb0
hrbcksq <hrbcksq@gmail.com>
2016-09-06 17:10:17 +0300

    Control validation pending state

--- app/usernameValidators.ts
+++ app/usernameValidators.ts
@@ -10,7 +10,7 @@ export class UsernameValidators{
                 else {
                     resolve(null);
                 }                    
-            });
+            }, 2000);
         })
     }
 
