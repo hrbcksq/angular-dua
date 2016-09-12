8aa4ebc24193c975a72a031024c56ad5d9b1d5e4
hrbcksq <hrbcksq@gmail.com>
2016-09-12 15:27:50 +0300

    Reactive observable constructors

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,7 @@
 import {Component} from 'angular2/core';
 import {ControlGroup, FormBuilder} from 'angular2/common';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/fromArray';
+import {Observable} from 'rxjs/Rx';
+
 
 @Component({
     selector: 'my-app',
@@ -15,22 +11,10 @@ export class AppComponent {
     public form: ControlGroup;
 
     constructor(fb: FormBuilder) {
-        var dates = [1, 2, 3];
-        
-
-        var startDates = [];
-        var startDate = new Date();
-
-        for (var day = -2; day <= 2; day++) {
-            var date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDay() + day)
-            startDates.push(date);
-        }
-
-        Observable.fromArray(startDates)
-            .map(date => {
-                console.log('Getting deals for date ' + date)
-                return [1,2,3];
-            })
+        // Observable.empty()
+        // Observable.range(1, 5)
+        // Observable.fromArray([1, 2, 3])
+        Observable.of([1, 2, 3])
             .subscribe(x => console.log(x));
     }
 }
