d1c8bfd18ae9eeaeb354ec61cd65533beeda4f85
hrbcksq <hrbcksq@gmail.com>
2016-09-12 15:13:15 +0300

    Reactive exercise 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,29 +5,32 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/fromEvent';
-
-
+import 'rxjs/add/observable/fromArray';
 
 @Component({
-    selector: 'my-app',    
-    template: `
-            <form [ngFormModel]='form'>
-                <input type='text' ngControl='search'>
-            </form>
-        `    
+    selector: 'my-app',
+    template: ``
 })
 export class AppComponent {
     public form: ControlGroup;
 
-    constructor(fb: FormBuilder){
-        this.form = fb.group({
-            search: []
-        });
+    constructor(fb: FormBuilder) {
+        var dates = [1, 2, 3];
+        
 
-        var search = this.form.find('search');
-        search.valueChanges
-            .debounceTime(400)
-            .map(str => (<string>str).replace(' ','-'))
+        var startDates = [];
+        var startDate = new Date();
+
+        for (var day = -2; day <= 2; day++) {
+            var date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDay() + day)
+            startDates.push(date);
+        }
+
+        Observable.fromArray(startDates)
+            .map(date => {
+                console.log('Getting deals for date ' + date)
+                return [1,2,3];
+            })
             .subscribe(x => console.log(x));
-    }    
+    }
 }
