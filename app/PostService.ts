47b14a55d95079b943f19be937bac1a0d36b9b7d
hrbcksq <hrbcksq@gmail.com>
2016-09-15 21:01:42 +0300

    Strong typing

--- app/PostService.ts
+++ app/PostService.ts
@@ -1,18 +1,20 @@
 import {Http} from 'angular2/http';
 import {Injectable} from 'angular2/core';
+import {Post} from './Post'
 import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs'
 
 @Injectable()
 export class PostService{
-	private urlsPost = 'https://jsonplaceholder.typicode.com/posts'
+	private urlsPost = 'https://jsonplaceholder.typicode.com/posts';
 
 	constructor(private _http: Http){ }	
 
-	getPosts(){
+	getPosts(): Observable<Post[]> {
 		return this._http.get(this.urlsPost).map(x => x.json());
 	}
 
-	postPost(post){
+	postPost(post: Post){
 		return this._http.post(this.urlsPost, JSON.stringify(post)).map(x => x.status);
 	}
 }
